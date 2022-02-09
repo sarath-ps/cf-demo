63db6895b5f9ea92a94e13b8488610692a64f458
Sarath P S <hello@sarath.info>
2022-02-09 14:21:34 +0530

    Initial commit

---
+++ src/main/java/com/example/cfdemo/CfDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.cfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfDemoApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/com/example/cfdemo/CfDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cfdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
